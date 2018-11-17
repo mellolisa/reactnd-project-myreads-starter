ab5eefaf35a8a746f6a34171938a3fa32d25b60b
Lisa M. Venezia <leftylmv78@gmail.com>
2018-11-17 11:38:29 -0500

    feature: Add links for open and close search

--- src/components/SearchBtn.js
+++ src/components/SearchBtn.js
@@ -1,19 +1,11 @@
 import React from "react";
-import { Route } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 class SearchBtn extends React.Component {
   render() {
     return (
       <div className="open-search">
-        <a
-          onClick={() =>
-            this.setState({
-              showSearchPage: true
-            })
-          }
-        >
-          Add a book{" "}
-        </a>{" "}
+        <Link to="/search">Add a book</Link>
       </div>
     );
   }
--- src/components/pages/SearchPage.js
+++ src/components/pages/SearchPage.js
@@ -1,21 +1,14 @@
 import React from "react";
-import { Route } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 class SearchPage extends React.Component {
   render() {
     return (
       <div className="search-books">
         <div className="search-books-bar">
-          <a
-            className="close-search"
-            onClick={() =>
-              this.setState({
-                showSearchPage: false
-              })
-            }
-          >
-            Close{" "}
-          </a>{" "}
+          <Link className="close-search" to="/">
+            Close
+          </Link>{" "}
           <div className="search-books-input-wrapper">
             {" "}
             {/*
